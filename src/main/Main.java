c1dd229a262740509e27f0ef214dcdb2d405f3e8
shivam rajput <shivam.rajput_cs18@gmail.com>
2019-08-12 15:24:29 +0530

    main.java: add a program for "printing a table"

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,9 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
     }
-
 }
