5f2a1da68783f454544c837051907c5791d31c58
shivam rajput <shivam.rajput_cs18@gmail.com>
2019-08-12 15:46:42 +0530

    main.java: add statements for taking input of a number

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,12 @@
 
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int number = 5;
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " * " + i + " = " + (number * i));
         }
